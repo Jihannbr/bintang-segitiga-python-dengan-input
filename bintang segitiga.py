0918e4ca61bf091afc8d1a8f03ec35b5fa7ac72c
Jihan Nabila <nabila7m8@gmail.com>
2024-11-22 20:40:56 +0800

    Add files via upload

---
+++ bintang segitiga.py
@@ -0,0 +1,29 @@
+string = ""
+x = int(input("Masukkan angka: "))
+bar = x
+
+#Looping baris
+while bar >= 0:
+
+    #Looping kolom spasi kosong
+    kol = bar
+    while kol >= 0:
+        string = string +"   "
+        kol = kol - 1
+
+    #Looping kolom bintang sisi kiri
+    kiri = 1
+    while kiri < (x - (bar-1)):
+        string = string + " * "
+        kiri = kiri + 1
+
+    #Looping kolom bintang sisi kanan
+    kanan = 1
+    while kanan < kiri - 1:
+        string = string + " * "
+        kanan = kanan + 1
+
+    string = string + "\n\n"
+    bar = bar - 1
+
+print(string)
